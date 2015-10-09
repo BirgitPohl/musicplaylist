09f9a35ad29aa94f97dcc8a9d459712869c90a28
Tim Repke <timmothey@gmx.de>
2015-10-09 12:17:56 +0200

    cleaned up musicplayer js, removed competing setTimeouts/intervals

--- js/musicplayer.js
+++ js/musicplayer.js
@@ -1,124 +1,54 @@
-	// var pause = new Image();
-	// pause.src = "pause.jpg";
-
-// var play_control = 0;	
-// function playmusic() {
-// 	if (play_control == 0) {
-// 		// document.getElementById('audioPlayer').play();
-// 		audio.autoplay = true;
-// 		document.getElementById('playbutt').src = 'pause.jpg';
-// 		play_control = 1;
-// 		window.setTimeout("playcontrol()",0);
-// 		window.setTimeout("zeitanzeige()",0);
-// 	} else {
-// 		document.getElementById('audioPlayer').pause();
-// 		document.getElementById('playbutt').src = 'play.jpg';
-// 		//document.getElementById('playbutton').innerHTML = "<"img onclick=\"playmusic()\" id=\"playbutt\" src=\"play.jpg\">"
-
-// 		play_control = 0;
-// 	}
-
-
-
 function playmusic() {
-		// document.getElementById('audioPlayer').play();
 		audio.play();
-		document.getElementById('playbutton').innerHTML = "<img onclick=\"pause()\" id=\"playbutt\" src=\"pause.jpg\">";
-		window.setTimeout("playcontrol()",0);
-		window.setInterval("zeitanzeige()",0);
-		// play_control = 1;
+		document.getElementById('playbutton').innerHTML = '<img onclick="pause()\" id="playbutt" src="pause.jpg">';
+		playcontrol();
+        zeitanzeige();
 	}
 function pause() {
-	// if (audio.played == true) {
 		audio.pause();
 		document.getElementById('playbutt').src = 'play.jpg';
-		// play_control = 0;
-		document.getElementById('playbutton').innerHTML = "<img onclick=\"playmusic()\" id=\"playbutt\" src=\"play.jpg\">"
-	// }
-
+		document.getElementById('playbutton').innerHTML = '<img onclick="playmusic()" id="playbutt" src="play.jpg">';
 }
 function playcontrol() {
-	// if(play_control == 1) {
-	if (audio.paused == false) {
-		if(audio.currentTime == audio.duration) {
-		document.getElementById('playbutt').src = 'play.jpg';
-		// play_control = 0;
-		} else {
-		window.setTimeout("playcontrol()",0);
+	var loop = setInterval(function() {
+		if (audio.paused === false && audio.currentTime === audio.duration) {
+            document.getElementById('playbutt').src = 'play.jpg';
+            clearInterval(loop);
 		}
-	}
+	}, 500);
+
 }
 function zeitanzeige() {
-	if (audio.paused == false) {
-	// if(play_control == 1) {
-		var full = audio.duration;
-		var full_min = Math.floor(full / 60);
-		var full_sec = Math.floor(full - (full_min *60));
-		
-		if(full_min < 10) {
-			full_min = '0' + full_min;
-		}
-		if(full_sec < 10) {
-			full_sec = '0' + full_sec;
-		}
-		
-		var curr = audio.currentTime;
-		var curr_min = Math.floor(curr / 60);
-		var curr_sec = Math.floor(curr - (curr_min *60));
-		
-		if(curr_min < 10) {
-			curr_min = '0' + curr_min;
-		}
-		if(curr_sec < 10) {
-			curr_sec = '0' + curr_sec;
-		}
-		
-		document.getElementById('time').innerHTML = ""+curr_min+":"+curr_sec+"/"+full_min+":"+full_sec+"";
-		window.setTimeout("zeitanzeige()",0);
-	}
+    var loop = setInterval(function() {
+        if (audio.paused == false) {
+            var full = prettyTime(audio.duration);
+            var curr = prettyTime(audio.currentTime);
+
+            document.getElementById('time').innerHTML = curr + '/' + full;
+        } else {
+            clearInterval(loop);
+        }
+    }, 500);
+
+    function prettyTime(timestamp) {
+        var min = Math.floor(timestamp / 60);
+        var sec = Math.floor(timestamp - (min *60));
+
+        min = prefixZero(min);
+        sec = prefixZero(sec);
+
+        return min + ':' + sec;
+
+        function prefixZero(time) {
+            return ((time < 10) ? '0' : '') + time;
+        }
+    }
 }
 function vol(z) {
-	switch(z) {
-		case "1":
-		audio.volume = 0.2;
-		document.getElementById('vol1').style.background = '#c9c3c3';
-		document.getElementById('vol2').style.background = '#8d8585';
-		document.getElementById('vol3').style.background = '#8d8585';
-		document.getElementById('vol4').style.background = '#8d8585';
-		document.getElementById('vol5').style.background = '#8d8585';
-		break;
-		case "2":
-		audio.volume = 0.4;
-		document.getElementById('vol1').style.background = '#c9c3c3';
-		document.getElementById('vol2').style.background = '#c9c3c3';
-		document.getElementById('vol3').style.background = '#8d8585';
-		document.getElementById('vol4').style.background = '#8d8585';
-		document.getElementById('vol5').style.background = '#8d8585';		
-		break;	
-		case "3":
-		audio.volume = 0.6;
-		document.getElementById('vol1').style.background = '#c9c3c3';
-		document.getElementById('vol2').style.background = '#c9c3c3';
-		document.getElementById('vol3').style.background = '#c9c3c3';
-		document.getElementById('vol4').style.background = '#8d8585';
-		document.getElementById('vol5').style.background = '#8d8585';			
-		break;			
-		case "4":
-		audio.volume = 0.8;
-		// document.getElementById('audioPlayer').volume = 0.6;
-		document.getElementById('vol1').style.background = '#c9c3c3';
-		document.getElementById('vol2').style.background = '#c9c3c3';
-		document.getElementById('vol3').style.background = '#c9c3c3';
-		document.getElementById('vol4').style.background = '#c9c3c3';
-		document.getElementById('vol5').style.background = '#8d8585';			
-		break;	
-		case "5":
-		audio.volume = 1.0;
-		document.getElementById('vol1').style.background = '#c9c3c3';
-		document.getElementById('vol2').style.background = '#c9c3c3';
-		document.getElementById('vol3').style.background = '#c9c3c3';
-		document.getElementById('vol4').style.background = '#c9c3c3';
-		document.getElementById('vol5').style.background = '#c9c3c3';			
-		break;			
-	}
+    var bars = ['vol1', 'vol2', 'vol3', 'vol4', 'vol5'];
+    var volume = parseInt(z);
+    bars.forEach(function(id, i) {
+        document.getElementById(id).style.background = (i < volume) ? '#c9c3c3' : '#8d8585';
+    });
+    audio.volume = volume * 0.2;
 }
